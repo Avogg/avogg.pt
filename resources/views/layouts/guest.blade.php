8e7e912c9164e6e54bc66357a4f9902a2e29206b
vascomartins <vascomartins37@gmail.com>
2022-01-28 11:37:22 +0000

    Add google analytics

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,6 +17,17 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-218775843-1">
+        </script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-218775843-1');
+        </script>
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
